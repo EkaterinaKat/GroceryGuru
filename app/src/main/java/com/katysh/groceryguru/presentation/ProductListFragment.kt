da1b198be6c4ba2a2a50df195e8debf2fde5e522
EkaterinaKat <Katerina290896@mail.ru>
2025-08-14 11:35:39 +0500

    кнопка "X" в списке типов еды

--- app/src/main/java/com/katysh/groceryguru/presentation/ProductListFragment.kt
+++ app/src/main/java/com/katysh/groceryguru/presentation/ProductListFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.katysh.groceryguru.GroceryGuruApplication
+import com.katysh.groceryguru.R
 import com.katysh.groceryguru.databinding.FragmentProductListBinding
 import com.katysh.groceryguru.model.ProductType
 import com.katysh.groceryguru.model.ProductWithPortions
@@ -127,6 +128,17 @@ class ProductListFragment : Fragment() {
             }
             binding.typeLayout.addView(button)
         }
+
+        val button = Button(requireActivity())
+        button.text = "X"
+        button.setBackgroundResource(R.color.white)
+        val params = LinearLayout.LayoutParams(0, WRAP_CONTENT, 1.0f)
+        button.layoutParams = params
+        button.setOnClickListener {
+            selectedType = null
+            updateProductList()
+        }
+        binding.typeLayout.addView(button)
     }
 
     interface OnProductClickListener {
